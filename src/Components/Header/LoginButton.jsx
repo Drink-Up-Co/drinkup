76c774b1d198656ab0aa8d80426bf2366f7a9de1
Diego Vazquez <diegovazquezny@gmail.com>
2020-11-17 19:01:54 -0500

    Add main folder for cards

--- src/Components/Header/LoginButton.jsx
+++ src/Components/Header/LoginButton.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from '@material-ui/core';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    btn: {
+      marginRight: '40px'
+    },
+  }),
+);
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
+  const classes = useStyles();
+
 
   return (
     <Button 
       onClick={() => loginWithRedirect()}
       variant="contained"
       size="large"
-      color="primary" 
+      color="primary"
+      className={classes.btn}
     >
       Log In
     </Button>
