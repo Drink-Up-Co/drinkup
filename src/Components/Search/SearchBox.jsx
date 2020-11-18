3798d4eb8ac169b702067a41b0d18d7785ad4f72
Diego Vazquez <diegovazquezny@gmail.com>
2020-11-18 18:46:54 -0500

    Fix warning when using search box

--- src/Components/Search/SearchBox.jsx
+++ src/Components/Search/SearchBox.jsx
@@ -108,6 +108,7 @@ const SearchBox = () => {
           options={options.sort((a, b) => -b.firstLetter.localeCompare(a.firstLetter))}
           groupBy={(option) => option.firstLetter}
           getOptionLabel={(option) => option.name}
+          getOptionSelected={(option, value) => option.name === value.name}
           style={{ width: 300 }}
           renderInput={(params) => <TextField {...params} label="Ingredients" variant="outlined" ref={textFieldRef}/>}
         />
